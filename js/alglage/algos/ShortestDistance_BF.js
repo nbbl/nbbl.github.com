d72c01b88a7bbcb25ba5e834d762556c619ec5d8
juffel <julian.dobmann@googlemail.com>
2013-03-15 22:33:12 +0100

    ShortestDistance gefixt

--- js/alglage/algos/ShortestDistance_BF.js
+++ js/alglage/algos/ShortestDistance_BF.js
@@ -2,42 +2,46 @@ importScripts('../core/Core.js');
 
 self.onmessage = function(event) {
     var points = event.data.points;
-    var name = event.data.name;
 
     // durch die Serialisierung in eine Nachricht haben die Punkte ihre
     // Methoden verloren. Daher zurückcasten!
     points = Vector.cast(points);
 
-    calculate(points, name);
+    calculate(points);
 };
 
 // dieser Algorithmus berechnet brute-force die kürzeste Distanz aller
 // übergebenen Punkte in *points* aus, und gibt den Betrag der kürzesten
 // Distanz und eine Liste von Edge-Objekten aller kürzesten Paare von
 // Punkten zurück
-function calculate(points, name) {
+function calculate(points) {
     var shortestDistance = Infinity;
-    var edges;
+    var edges = [];
 
-    for (p in points) {
-        for (q in points) {
-            if (p !== q && p.distance(q) <= shortestDistance) {
-                shortestDistance = p.distance(q);
-                edges.push(new Edge(p, q));
+    for(var i = 0; i < points.length; i++) {
+        for(var j = i+1; j < points.length; j++) {
+
+            var dist = Vector.distance(points[i], points[j]);
+            if(dist <= shortestDistance) {
+                shortestDistance = dist;
+                edges.push(new Edge(points[i], points[j]));
             }
         }
     }
 
     // zu viele Edges hinzugefügt, nur die beibehalten, die auch wirklich
     // die kürzeste Länge haben
-    for (e in edges) {
-        if (! e.length() > shortestDistance)
-            edges = edges.splice(edges.indexOf(e), 1);
+    var result = [];
+    for(var i = 0; i < edges.length; i++) {
+        if (edges[i].length() <= shortestDistance) {
+            result.push(edges[i]); 
+        }
     }
 
     self.postMessage({
         score : shortestDistance,
-        more : edges,
-        name : 'ShortestDistance'
+        more : result,
+        name : 'ShortestDistance_BF',
+        info : ''
     });
 };
