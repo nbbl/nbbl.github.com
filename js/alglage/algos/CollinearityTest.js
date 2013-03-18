b3b54aed7910d7cbb9a41692b47313a4bd330993
Avealx <avealx@zedat.fu-berlin.de>
2013-03-18 11:49:50 -0300

    Update CollinearityTest.js

--- js/alglage/algos/CollinearityTest.js
+++ js/alglage/algos/CollinearityTest.js
@@ -1,7 +1,7 @@
 importScripts('../core/Core.js');
-importScripts('./Utilities.js');
+importScripts('../Utilities.js');
 
-CollinerarityTest_SPATIAL_TOLERANCE = 0.1;
+CollinearityTest_SPATIAL_TOLERANCE = 0.1;
 
 self.onmessage = function(event) {
     var points = Point.cast(event.data.points);
@@ -14,7 +14,7 @@ function calculate(points,name){
     var collintriple = [];
     for(var triple in sets){
         var edge = new Edge(triple[0],triple[1]);
-	if(edge.distanceToLine(triple[2]) < CollinerarityTest_SPATIAL_TOLERANCE) collintriple.push(edge); 
+	if(edge.distanceToLine(triple[2]) < CollinearityTest_SPATIAL_TOLERANCE) collintriple.push(edge); 
 	//später evtl alle Kanten/Punkt-kombos checken
     }
     self.postMessage({
