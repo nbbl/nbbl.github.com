3f4365366757590feffc9a3a3153371d5b5b4b51
avealx <avealx@zedat.fu-berlin.de>
2013-03-18 10:26:57 -0400

    'ShortestDistance_BF.js' straightLine -> lineSegment

--- js/alglage/algos/CollinearityTest.js
+++ js/alglage/algos/CollinearityTest.js
@@ -13,14 +13,15 @@ function calculate(points,name){
     var sets = new Subsets(points,3);
     var collintriple = [];
     for(var triple in sets){
-	if(new Edge(triple[0],triple[1]).distanceToLine(triple[2]) < CollinerarityTest_SPATIAL_TOLERANCE) collintriple.push(triple); 
+        var edge = new Edge(triple[0],triple[1]);
+	if(edge.distanceToLine(triple[2]) < CollinerarityTest_SPATIAL_TOLERANCE) collintriple.push(edge); 
 	//später evtl alle Kanten/Punkt-kombos checken
     }
     self.postMessage({
         score :  collintriple.length,
         annotations  : { 
-            lines : collintriple.map(function(x){return x.splice(0,2)});
-        }
+            lines : collintriple 
+        },
         name  :  name,
 	info  : ''
     });
