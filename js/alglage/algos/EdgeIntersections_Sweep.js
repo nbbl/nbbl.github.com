722a31a7d665565b1b01f4de0f2d743ca5f37c95
avealx <avealx@zedat.fu-berlin.de>
2013-03-26 09:06:15 -0400

    EdgeIntesections_Sqeep weitergemacht

--- js/alglage/algos/EdgeIntersections_Sweep.js
+++ js/alglage/algos/EdgeIntersections_Sweep.js
@@ -1,40 +1,77 @@
 function sweep(edges) {
     var sls = [];
     var evq = [];
-    
+
     for(var i=0; i<edges.length; ++i){
-	evq.push(new Event("start",edges[i]));
-	evq.push(new Event("end",edges[i]));
+        evq.push(new Event("start",edges[i]));
+        evq.push(new Event("end",edges[i]));
     }
 
     evq.sort(function(x,y){
-	return x.key - y.key;
+        return x.key - y.key;
     });
-    
-    var currev, prevev = null;
-    var commonOrdinate = false;
+
+    var currev = null;
     while(evq.length !== 0){
-	prevev = currev;
-	currev = evq.shift();
-	switch currev.type {
-	case "start":
-	    sls.push(currev.edge1);
-	    sls.sort(compareOrdinate); //<------------------
-	    break;
-	case "intersection":
-	    sls.push();
-	    break;
-	case "end":
-	    break;
-	}
+        currev = evq.shift();
+        switch currev.type {
+            case "start":
+                sls.push(currev.edge1);
+                sort_sls();
+                insertNeighbourIntersections(sls.indexOf(currev.edge1));
+                break;
+            case "intersection":
+
+                break;
+            case "end":
+                break;
+        }
     }
 
-    function compareOrdinate(x,y,key){ //<-------------------
-	var comp = null;
-	if ((comp = x.getY(key) -y.getY(key)) === 0){
-	    commonOrdinate = true;
-	}
-	return comp; 
+    function sort_sls(){
+        var commonOrdinate = false;
+        var iteration      = 0;
+        var DELTA          = 1E-5;
+
+        do {
+            commonOrdinate = false;
+            sls.sort(function(x,y){
+                var res = null;
+                if ((res = x.getY(currev.key+iteration*DELTA) - y.getY(currev.key+iteration*DELTA)) === 0) {
+                    commonOrdinate = true;
+                }
+                return res;
+            });
+            iteration++;
+        } while(commonOrdinate);
+    };
+
+    function insertNeighbourIntersections(index) {
+        var intersection1, intersection2 = null;
+        var inserted = false;
+        if (index<0) {
+            alert("EdgeInterseciton_Sweep: some edge was not inserted into sls!!");
+        } else {
+            if (index > 0 &&
+                (intersection1 = sls[index].edgeIntersection(sls[index-1])) !== null &&
+                !intersection1.equals(sls[index].pt1)   && 
+                !intersection1.equals(sls[index].pt2)   && 
+                !intersection1.equals(sls[index-1].pt1) && 
+                !intersection1.equals(sls[index-1].pt2) ) {
+                    evq.push(new Event("intersection",sls[index-1],sls[index],intersection1));
+                    inserted = true;
+            }
+            if (index < sls.length-1 &&
+                (intersection2 = sls[index].edgeIntersection(sls[index+1])) !== null &&
+                !intersection2.equals(sls[index].pt1)   && 
+                !intersection2.equals(sls[index].pt2)   && 
+                !intersection2.equals(sls[index+1].pt1) && 
+                !intersection2.equals(sls[index+1].pt2) ) {
+                    evq.push(new Event("intersection",sls[index],sls[index+1],intersection2));
+                    inserted = true;
+            }
+            if (inserted) evq.sort(function(x,y){return x.key-y.key;});
+        }
     };
 }
 
@@ -45,17 +82,17 @@ function Event(type,edge) {
 
     //this.key ist die X-Koordinate des Eventpoints
     switch type {
-    case "start": 
-	this.key = edge.getLeft().x;
-	break;
-    case "intersection":
-	this.key = args[3];
-	this.edge2 = args[2];
-	break;
-    case "end":
-	this.key = edge.getRight().x;
-	break;
+        case "start": 
+            this.key = edge.getLeft().x;
+            break;
+        case "intersection":
+            this.key = args[3].x;
+            this.edge2 = args[2];
+            break;
+        case "end":
+            this.key = edge.getRight().x;
+            break;
     }
-    
+
     this.edge1 = edge;
 }
