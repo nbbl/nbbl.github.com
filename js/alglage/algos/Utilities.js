c688182aae0397ec1fa88192629f7592580920e7
Johannes Sauer <jtsauer@gmail.com>
2013-03-27 15:09:27 +0000

    Array.remove() hizugefügt

--- js/alglage/algos/Utilities.js
+++ js/alglage/algos/Utilities.js
@@ -120,3 +120,10 @@ function ChainElem(elem) {
 /*------------------------------------------------------------------*/
 /*END Sorted Double Linked List-----------------------------------*/
 /*------------------------------------------------------------------*/
+
+
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
